6cdeaa92c6207aae9dee52b818a371814bb4f66f
mostafa <me.cssign@gmail.com>
2021-07-03 19:04:04 +0200

    Initialize a maven project

---
+++ src/main/java/mostafaism/com/github/App.java
@@ -0,0 +1,13 @@
+package mostafaism.com.github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
